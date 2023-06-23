54a7fc2ff98b0db9e33f41b9e56c646f2c5f2239
hsh0517 <pelino0517@gmail.com>
2023-06-23 11:31:45 +0900

    :hammer: :: Add GAuthForUnity CSharp Project

---
+++ GAuthForUnity/GAuthForUnity/GAuthCode.cs
@@ -0,0 +1,12 @@
+﻿namespace GAuthForUnity
+{
+    public class GAuthCode
+    {
+        public string Code { get; private set; }
+
+        public GAuthCode(string code)
+        {
+            Code = code;
+        }
+    }
+}
